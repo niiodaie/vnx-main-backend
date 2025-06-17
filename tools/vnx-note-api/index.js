efc98df49c12fbb1fd509a5c01471573b9d48a3d
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-17 02:16:05 -0500

    Update index.js

--- tools/vnx-note-api/index.js
+++ tools/vnx-note-api/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,9 +5,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("vnx-note API is live!");
-});
+// Mount API namespace
+const vnxNoteRoutes = require('./routes/vnxnote');
+app.use('/tools/vnx-note-api', vnxNoteRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
