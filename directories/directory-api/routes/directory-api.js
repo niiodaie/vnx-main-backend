97e67bb00072d4fda68b6783d08628297c17b282
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-17 20:58:40 -0500

    Add files via upload

--- directories/directory-api/routes/directory-api.js
+++ directories/directory-api/routes/directory-api.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.send("Directory api API is working!");
+});
+
+module.exports = router;
