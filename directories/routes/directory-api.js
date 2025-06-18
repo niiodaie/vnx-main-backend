129f646dbfbe5de0b83f0f547632b21013ff4c6b
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-17 20:51:10 -0500

    Delete directories directory

--- directories/routes/directory-api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send("Directory api API is working!");
-});
-
-module.exports = router;
