8db506baa5011f37fd8479da4600212b5b39d8be
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-17 20:51:42 -0500

    Delete experiences directory

--- experiences/routes/experience-api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.send("Experience api API is working!");
-});
-
-module.exports = router;
